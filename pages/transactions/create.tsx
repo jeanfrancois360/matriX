35cce738f6052aefabbe0cd2edb90629b5692aa9
Jean Francois Munyaneza <mjeanfrancois360@gmail.com>
2022-01-20 09:29:41 +0200

    Modified send money UI

--- pages/transactions/create.tsx
+++ pages/transactions/create.tsx
@@ -2,10 +2,13 @@ import { useEffect, useState } from 'react'
 import Header from '../../components/navbar'
 import * as Yup from 'yup'
 import axios from 'axios';
-import { useFormik } from 'formik'
+import { Formik, useFormik } from 'formik'
 import { CircularProgress } from '@mui/material';
 import Router from 'next/router';
 import Head from 'next/head';
+import Link from 'next/link';
+import ErrorMessage from '../../components/messages/ErrorMessage';
+import styles from '../../styles/Home.module.css';
 
 interface Transaction {
     receiverId: number,
@@ -17,7 +20,7 @@ interface Transaction {
 function Create(props:any) {
     const [users, setUsers] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [isTransactionProcessing, setIsTransactionProcessing] = useState(false);
+    const [isProcessing, setIsProcessing] = useState(false);
     const [senderId, setSenderId] = useState(-1)
     const [token, setToken] = useState('');
 
@@ -51,30 +54,25 @@ function Create(props:any) {
         }
         getUsers();
     }, []);
-    const { handleSubmit, handleChange, values, touched, errors, handleBlur } = useFormik({
-        initialValues: {
-            receiverId: -1,
-            target_currency: '',
-            source_currency: '',
-            amount: 0.0,
-        },
-        validationSchema: Yup.object({
-            receiverId: Yup.string().required(),
-            target_currency: Yup.string().required(),
-            source_currency: Yup.string().required(),
-            amount: Yup.number().required(),
-        }),
-        onSubmit: async ({ receiverId, source_currency, target_currency, amount }) => {
-            setIsTransactionProcessing(true);
-            const result = await axios.post('/api/transactions/create', { receiverId, source_currency, target_currency, amount, senderId }, { headers : { 'Authorization' : `Bearer ${token}` }});
 
-            const { data } = result.data;
-            setIsTransactionProcessing(false);
+  // Send money validations
+  const sendMoneyValidationSchema = Yup.object().shape({
+    receiverId: Yup.string().required().label("Receiver"),
+    target_currency: Yup.string().required().label("Target currency"),
+    source_currency: Yup.string().required().label("Source currency"),
+    amount: Yup.number().required().label("Amount"),
+  });
+
+    const handleSendMoney = async (values:object) => {
+        console.log("DATA: ", values)
+        return;
+        setIsProcessing(true);
+            const res = await axios.post('/api/transactions/create', { ...values }, { headers : { 'Authorization' : `Bearer ${token}` }});
+            setIsProcessing(false);
             alert('Transaction Successful')
             Router.push('/transactions');
-            console.log(data);
-        }
-    });
+            console.log("New transaction: ", res);
+    }
     return (
         <>
         <Head>
@@ -84,58 +82,93 @@ function Create(props:any) {
             <Header />
             {isLoading ? 
             <div className='flex flex-col items-center content-center pt-12'>
-                <CircularProgress /> 
+                <CircularProgress className={styles.pageLoaderStyle}/> 
             </div>
             :
 
-                <div className='flex flex-col items-center gap-16 p-24'>
-                    <p className='text-center font-bold text-xl'>Fill out this form to send money</p>
-                    <form onSubmit={handleSubmit} className='flex flex-col gap-8 bg-white shadow-md p-8 mb-4 rounded'>
+                <div className='flex flex-col items-center gap-16 p-24 w-full'>
+                    <div className='flex w-full items-center justify-between content-end bg-white p-5 rounded-2xl'>
+                    <p className='text-slate-900 font-bold'>Send money</p>
+                    <Link href="/transactions">
+                        <a className='px-8 py-3 bg-slate-900 text-white rounded-full hover:text-emerald-400'>View Transactions</a>
+                    </Link>
+                </div>
+                <Formik
+                initialValues={{
+                receiverId: "",
+                target_currency: '',
+                source_currency: '',
+                amount: "",
+                }}
+                onSubmit={handleSendMoney}
+                validationSchema={sendMoneyValidationSchema}
+              >
+                {({
+                  values,
+                  handleChange,
+                  handleSubmit,
+                  touched,
+                  handleBlur,
+                  errors,
+                }) => (
+                    <form action="POST" onSubmit={handleSubmit} className='flex flex-col gap-8 bg-white shadow-md p-8 mb-4 rounded'>
                         <div className='flex gap-6'>
                             <div className='mb-4 w-full'>
                                 <label htmlFor="receiverId" className='block text-gray-500 text-sm font-bold mb-2'>Receiver</label>
-                                <select name='receiverId' onChange={handleChange} onBlur={handleBlur} className="form-select appearance-none block w-full px-3 py-3 px-6 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
+                                <select name='receiverId' value={values.receiverId} onChange={handleChange("receiverId")} onBlur={handleBlur("receiverId")} className="form-select appearance-none block w-full px-3 py-3 px-6 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
                                     <option defaultValue={''}>Select Receiver</option>
                                     {users.length && users.map((user) => (
                                         <option key={user['id']} value={user['id']}>{user['name']}</option>
                                     ))}
                                 </select>
-                                {touched.receiverId && errors.receiverId ? <span className='text-red-700'>{errors.receiverId}</span> : ''}
+                                {touched.receiverId && errors.receiverId && <ErrorMessage
+                                            text={errors.receiverId}
+                                        />}
                             </div>
-                            <div className='mb-4 w-full'>
-                                <label htmlFor="t_currency" className='block text-gray-500 text-sm font-bold mb-2'>Target currency</label>
-                                <select name='target_currency' onChange={handleChange} onBlur={handleBlur} className="form-select appearance-none block w-full px-3 py-3 px-6 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
-                                    <option selected>choose target currency</option>
-                                    <option value="USD">USD</option>
-                                    <option value="NGN">NGN</option>
-                                    <option value="EUR">EUR</option>
-                                </select>
-                                {touched.target_currency && errors.target_currency ? <span className='text-red-700'>{errors.target_currency}</span> : ''}
-                            </div>
-                        </div>
-                        <div className='flex gap-6 mb-4'>
                             <div className='mb-4 w-full'>
                                 <label htmlFor="s_currency" className='block text-gray-500 text-sm font-bold mb-2'>Source currency</label>
-                                <select name='source_currency' onChange={handleChange} onBlur={handleBlur} className="form-select appearance-none block w-full px-3 py-3 px-6 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
+                                <select name='source_currency' value={values.source_currency} onChange={handleChange("source_currency")} onBlur={handleBlur("source_currency")} className="form-select appearance-none block w-full px-3 py-3 px-6 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
                                     <option selected>choose source currency</option>
                                     <option value="USD">USD</option>
                                     <option value="NGN">NGN</option>
                                     <option value="EUR">EUR</option>
                                 </select>
-                                {touched.source_currency && errors.source_currency ? <span className='text-red-700'>{errors.source_currency}</span> : ''}
+                                {touched.source_currency && errors.source_currency && <ErrorMessage
+                                            text={errors.source_currency}
+                                        />}
+                            </div>
+                           
+                        </div>
+                        <div className='flex gap-6 mb-4'>
+                             <div className='mb-4 w-full'>
+                                <label htmlFor="t_currency" className='block text-gray-500 text-sm font-bold mb-2'>Target currency</label>
+                                <select name='target_currency' value={values.target_currency} onChange={handleChange("target_currency")} onBlur={handleBlur("target_currency")} className="form-select appearance-none block w-full px-3 py-3 px-6 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
+                                    <option selected>choose target currency</option>
+                                    <option value="USD">USD</option>
+                                    <option value="NGN">NGN</option>
+                                    <option value="EUR">EUR</option>
+                                </select>
+                                {touched.target_currency && errors.target_currency && <ErrorMessage
+                                            text={errors.target_currency}
+                                        />}
                             </div>
                             <div className='mb-4 w-full'>
                                 <label htmlFor="amount" className='block text-gray-500 text-sm font-bold mb-2'>Amount</label>
-                                <input type="number" value={values.amount} name='amount' onChange={handleChange} onBlur={handleBlur} placeholder='Enter amount' className='shadow appearance-none border rounded w-full py-3 px-6 text-gray-700 focus:shadow-outline' />
-                                {touched.amount && errors.amount ? <span className='text-red-700'>{errors.amount}</span> : ''}
+                                <input type="number" min="0" value={values.amount} name='amount' onChange={handleChange("amount")} onBlur={handleBlur("amount")} placeholder='Enter amount' className='shadow appearance-none border rounded w-full py-3 px-6 text-gray-700 focus:shadow-outline' />
+                                {touched.amount && errors.amount && <ErrorMessage
+                                            text={errors.amount}
+                                        /> }
                             </div>
                         </div>
-                        <div className='flex items-center justify-between'>
-                            {
-                                <button type="submit" className='inline-flex justify-center text-white font-bold hover:text-blue-400 bg-slate-700 hover:bg-slate-900 py-2 px-4 rounded'>{ isTransactionProcessing ? <CircularProgress /> : 'PROCEED' }</button>
-                            }
+                        <div className='flex items-center justify-center'>
+                            <button className="bg-slate-900 my-4 mx-0 px-6 py-3 w-40 text-emerald-400 rounded-full hover:text-white">
+                   {isProcessing ? <CircularProgress className={styles.loaderStyle} /> : 'Send money'}
+                  </button>
+                           
                         </div>
-                    </form>
+                   </form>
+                )}
+              </Formik>
                 </div>
             }
         </>
